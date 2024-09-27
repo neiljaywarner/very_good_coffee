187b4c358ac34a7856dab4be48486fee511487ab
neil warner <njwandroid@gmail.com>
2024-09-26 23:43:57 -0500

    2 - fix tests to use fake repository via riverpod di.

--- test/bdd_steps/the_app_is_running.dart
+++ test/bdd_steps/the_app_is_running.dart
@@ -2,11 +2,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:very_good_coffee/app/view/app.dart';
+import 'package:very_good_coffee/home/network/photos_repository.dart';
 
 Future<void> theAppIsRunning(WidgetTester tester) async {
   await mockNetworkImages(
     () async => await tester.pumpWidget(
-      const ProviderScope(
+      ProviderScope(
+        overrides: [photosRepositoryProvider.overrideWithValue(FakePhotosRepository())],
         child: App(),
       ),
     ),
